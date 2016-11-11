13c8d8a81dfe66b8e902cbe962942e0c582987bd
nathan rogers <nathan rogers>
2016-11-10 17:18:12 -0700

    created core funcationality for equipement.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    template: '<h1>My First Angular App</h1><all-equipment></all-equipment>'
 })
 export class AppComponent { }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import {EquipmentComponent} from './EquipmentComponents/equipment.component';
+import {EquipmentsComponent} from './EquipmentComponents/equipments.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  declarations: [ 
+    AppComponent, 
+    EquipmentComponent,
+    EquipmentsComponent ],
+  bootstrap: [ AppComponent ],
+  providers: []
 })
+
 export class AppModule { }
